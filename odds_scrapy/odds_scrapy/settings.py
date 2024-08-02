87e36354297ee82a00e3dce85f4f962f6598e7d3
atsuki-3590 <atsukiperc@gmail.com>
2024-08-02 13:35:47 +0900

    scrapyでのコードの追加

---
+++ odds_scrapy/odds_scrapy/settings.py
@@ -0,0 +1,17 @@
+# settings.py
+
+BOT_NAME = 'odds_scrapy'
+SPIDER_MODULES = ['odds_scrapy.spiders']
+NEWSPIDER_MODULE = 'odds_scrapy.spiders'
+
+# 適切なUSER_AGENTを設定
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+
+# 適度なダウンロードディレイを設定
+DOWNLOAD_DELAY = 1
+
+# 同時リクエスト数を設定
+CONCURRENT_REQUESTS = 20
+
+# Robots.txtの遵守を無効化
+# ROBOTSTXT_OBEY = False
